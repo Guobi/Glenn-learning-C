d5f83b5151f0a3084f5b6d201350c6d1372216ad
Guobi Wu <guobi.wu@autodesk.com>
2020-05-06 15:24:00 +0800

    update a bit

--- c_prg_ex1.c
+++ c_prg_ex1.c
@@ -1,17 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h> 
+#include <time.h>
 
 void diceRoll(int rolls);
 
-void main() {
-    srand(time(0)); // set rng seed as current time
-    diceRoll(100); // generates number from 1 - 6 for 100 times
+int main()
+{
+    diceRoll(10000000); // generates number from 1 - 6 for 100 times
+    return 0;
 }
 
-void diceRoll(int rolls) {
-    int i;
-    for (i = 0; i < rolls; i++) {
-        printf("%d\n",(rand() % 6) + 1);
+void diceRoll(int rolls)
+{
+    int i = 0;
+    int count[6];
+    for (i = 0; i < 6; i++)
+    {
+        count[i] = 0;
     }
+
+    // set rng seed as current time
+    srand(time(0));
+    int myRand = 0;
+    for (i = 0; i < rolls; i++)
+    {
+        myRand = (rand() % 6) + 1;
+        count[myRand - 1]++;
+        // printf("%d\n", myRand);
+    }
+    for (i = 0; i < 6; i++)
+    {
+        printf("%d\n", count[i]);
+    }
+    return;
 }
